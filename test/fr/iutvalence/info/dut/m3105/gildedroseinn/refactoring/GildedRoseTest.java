096bc2a1c7d48702eeb17efbbecec12cf9083e6d
blouin maxime <maxime.blouin@iut-valence.fr>
2015-11-03 14:51:23 +0100

    red 1

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -2,14 +2,25 @@ package fr.iutvalence.info.dut.m3105.gildedroseinn.refactoring;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class GildedRoseTest
 {
 	@Test
-	public void aSimpleBeginningTest()
+	public void decrementSellInTestForAnItems()
 	{
-		assertEquals(true,false);
+		ArrayList<Item> itemList;
+		
+		itemList = new ArrayList<Item>(); 
+		itemList.add(new Item("testOne", 50, 100));
+		
+		int valSellInDayOne = itemList.get(0).getSellIn();
+		
+		GildedRose.updateItems(itemList);
+		
+		assertEquals(valSellInDayOne-1, itemList.get(0).getSellIn());
 	}
 
 }
