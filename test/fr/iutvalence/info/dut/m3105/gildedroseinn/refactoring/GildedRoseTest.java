c3e626a1ef16623d9132d75b019a3ef0cf033c3c
blouin maxime <maxime.blouin@iut-valence.fr>
2015-11-03 15:23:02 +0100

    Red 3 Quality with negative sellIn

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -11,6 +11,7 @@ public class GildedRoseTest
 	
 	public static final Item ITEM = new Item("testOne", 50, 100);
 	public static final Item ITEM_TWO = new Item("testTwo", 70, 80);
+	public static final Item ITEM_THREE = new Item("testThree", 1, 8);
 	
 	@Test
 	public void decrementSellInTestForAnItem()
@@ -31,5 +32,20 @@ public class GildedRoseTest
 		GildedRose.updateItem(ITEM_TWO);
 		assertEquals(valQualityDayOne -1, ITEM_TWO.getQuality());
 	}
+	
+	@Test
+	public void decrementQualityForAnItemWhoHasASellInWhoBecameNegative(){
+		int valQualityDayOne = ITEM_THREE.getQuality();
+		int valQualityDayTwo;
+		
+		GildedRose.updateItem(ITEM_THREE);
+		valQualityDayTwo = ITEM_THREE.getQuality();
+		
+		assertEquals(valQualityDayOne -1, ITEM_THREE.getQuality());
+		
+		GildedRose.updateItem(ITEM_THREE);
+		assertEquals(valQualityDayTwo -2, ITEM_THREE.getQuality());
+	}
+
 
 }
